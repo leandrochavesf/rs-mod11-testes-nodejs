a46af45f10be32144231ae1d7a2f4842fa5588e0
Leandro Chaves <leandrochaves.f@gmail.com>
2019-12-03 20:41:26 -0300

    E-mail duplicado

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,17 +1,44 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  // Verificação de registro
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
-        id: '1',
         name: 'Diego Fernandes',
         email: 'diego@rocketseat.com.br',
         password_hash: '123456',
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  // Verificação de email duplicado
+  it('should be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@rocketseat.com.br',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
